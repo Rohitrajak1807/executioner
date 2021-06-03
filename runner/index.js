f18859d95a889856bce2517bfe08bb0684daa567
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-06-03 11:53:28 +0530

    add cors

--- runner/index.js
+++ runner/index.js
@@ -3,9 +3,11 @@ const {connectDB} = require('./conf/db')
 const app = require('express')()
 const {router} = require('./route/routes.js')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 app.use(bodyParser.json({
     limit: '50mb'
 }))
+app.use(cors())
 app.use(router)
 app.listen(PORT, HOST, () => {
     console.log(`listening on ${PORT}`)
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const {HOST, PORT} = require('./conf/env')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const {connectDB} = require('./conf/db')
 const app = require('express')()
 const {router} = require('./routes/core')
@@ -7,5 +8,6 @@ connectDB()
 app.use(bodyParser.json({
     limit: '50mb'
 }))
+app.use(cors())
 app.use(router)
 app.listen(PORT, HOST, () => console.log(`listening on ${PORT}`))
